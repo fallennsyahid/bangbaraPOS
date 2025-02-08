03a2c8fda12cffa8b01d020e82e808b506bdc59b
fallennsyahid <umarusyahidmasudi1@gmail.com>
2025-02-08 09:57:46 +0700

    update ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,9 +19,6 @@
         href="https://fonts.googleapis.com/css2?family=Euphoria+Script&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet" />
 
-    <!-- ICONS -->
-    {{-- <script src="https://unpkg.com/feather-icons"></script> --}}
-
 </head>
 
 <body>
@@ -481,8 +478,5 @@
     {{-- POP UP END --}}
 </body>
 <script src="{{ asset('asset-view/js/script.js') }}"></script>
-<script>
-    feather.replace();
-</script>
 
 </html>
