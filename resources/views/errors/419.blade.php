b13258ce68ecf4953cfc4ba19b3d8c1a1eec219a
fallennsyahid <umarusyahidmasudi1@gmail.com>
2025-04-25 14:59:15 +0700

    last pull?

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 - Halaman Tidak Ditemukan</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <section class="flex items-center h-screen p-16 bg-zinc-900">
+        <div class="container flex flex-col items-center ">
+            <div class="flex flex-col gap-6 max-w-md text-center">
+                <h2 class="font-extrabold text-9xl text-gray-600 dark:text-gray-100">
+                    <span class="sr-only">Error</span>419
+                </h2>
+                <p class="text-2xl md:text-3xl text-amber-300">Reload your browser</p>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
